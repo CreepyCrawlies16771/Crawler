d8c93711153ed2072e01099310a630caf47ed2d1
CreepyCrawlies16771 <108303195+CreepyCrawlies16771@users.noreply.github.com>
2026-04-02 14:55:27 +0200

    Add path-following utilities and math helpers
    Introduce path-following support and related math utilities.
    
    - Add CurvePoint and Point classes to represent path waypoints and 2D points.
    - Implement followCurve and getFollowPointPath in RobotMovement to pick a follow point on a polyline using a follow radius (line-circle intersections).
    - Update goToPosition signature to accept preferredAngle and turnSpeed and compute relative movement/turning powers.
    - Fix AngleWrap implementation (use degrees consistently), add Clip, and add lineCircleIntersection helper in MathFunctions.
    
    Notes: includes TODO comments for tuning follow distance/endpoint handling and reducing orbiting when near the target.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Crawler/FieldOrient/RobotMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Crawler/FieldOrient/RobotMovement.java
@@ -2,7 +2,13 @@ package org.firstinspires.ftc.teamcode.Crawler.FieldOrient;
 
 import org.firstinspires.ftc.teamcode.Crawler.core.CrawlerRobot;
 import org.firstinspires.ftc.teamcode.Crawler.core.Localizers.CrawlerLocaliser;
+import org.firstinspires.ftc.teamcode.Crawler.core.utils.CurvePoint;
 import org.firstinspires.ftc.teamcode.Crawler.core.utils.MathFunctions;
+import org.firstinspires.ftc.teamcode.Crawler.core.utils.Point;
+
+import java.util.ArrayList;
+
+import javax.annotation.Nullable;
 
 public class RobotMovement {
 
@@ -17,25 +23,91 @@ public class RobotMovement {
         worldAngle_radians = robot.devLocaliser.getPose().getHeading();
     }
 
+    //TODO: Fix when end appeards, extend the last line by more than the follow distance. Get close to the target point, to last point of the list.
+    public static void followCurve(ArrayList<CurvePoint> allPoints, double followAngle) {
+        for (int i = 0; i < allPoints.size() - 1; i++) {
+            /// Send information of line to debugger here (Lines)
+        }
 
+        CurvePoint followMe = getFollowPointPath(allPoints,
+                new Point(worldXPosition, worldYPosition),
+                allPoints.get(0).followDistance); // Todo: figure out the point on the path to set the best follow distance (Depending on speed and distance of the path...)
+        /// Display more stuff here
 
-    public static void goToPosition(double x, double y, double movementSpeed) {
+        goToPosition(followMe.x, followMe.y, followMe.moveSpeed, followAngle, followMe.turnSpeed);
+    }
+
+    /**
+     * This gets the best point from the path to follow/go to
+     * @param pathPoints
+     * @param robotLocation
+     * @param followRadius
+     * @return
+     */
+    public static CurvePoint getFollowPointPath(ArrayList<CurvePoint> pathPoints, Point robotLocation, double followRadius) {
+        CurvePoint followMe = new CurvePoint(pathPoints.get(0));
+
+        for(int i = 0; i < pathPoints.size() - 1; i++) {
+            CurvePoint startLine = pathPoints.get(i);
+            CurvePoint endLine = pathPoints.get(i +1);
+
+            ArrayList<Point> intersections = MathFunctions.lineCircleIntersection(robotLocation, followRadius, startLine.toPoint(), endLine.toPoint());
+
+            double closetsAngle = 10000000;
+
+            for(Point thisIntersection: intersections) {
+                double angel = Math.atan2(thisIntersection.y - worldYPosition, thisIntersection.x - worldXPosition);
+                double deltaAngle = Math.abs(MathFunctions.AngleWrap(angel - worldAngle_radians));
+
+                if(deltaAngle < closetsAngle) {
+                    closetsAngle = deltaAngle;
+                    followMe.setPoint(thisIntersection);
+                }
+
+            }
+        }
+
+        return followMe;
+    }
+
+    /**
+     * This makes the robot go to a point on the field
+     * @param x
+     * @param y
+     * @param movementSpeed
+     * @param preferredAngle
+     * @param turnSpeed
+     */
+
+    public static void goToPosition(double x, double y, double movementSpeed, double preferredAngle, double turnSpeed) {
 
         double distanceToTarget = Math.hypot(x-worldXPosition, y-worldYPosition);
 
-       double absAngleToTarget = Math.atan2(y - worldYPosition, x - worldXPosition);
+        double absAngleToTarget = Math.atan2(y - worldYPosition, x - worldXPosition);
 
-       double relativeAngleToPoint = MathFunctions.AngleWrap(absAngleToTarget - (worldAngle_radians - Math.toRadians(0)));
-
-
-       double relativeXToPoint = Math.cos(relativeAngleToPoint) * distanceToTarget;
-       double relativeYToPoint = Math.sin(relativeAngleToPoint) * distanceToTarget;
+        double relativeAngleToPoint = MathFunctions.AngleWrap(absAngleToTarget - (worldAngle_radians - Math.toRadians(0)));
 
 
-       double movementXPower = relativeXToPoint / (Math.abs(relativeXToPoint) + Math.abs(relativeYToPoint));
+        double relativeXToPoint = Math.cos(relativeAngleToPoint) * distanceToTarget;
+        double relativeYToPoint = Math.sin(relativeAngleToPoint) * distanceToTarget;
 
-       double movementYPower = relativeYToPoint / (Math.abs(relativeXToPoint) + Math.abs(relativeYToPoint));
 
-        //todo: get the data for the movement from here
+        double movementXPower = relativeXToPoint / (Math.abs(relativeXToPoint) + Math.abs(relativeYToPoint));
+
+        double movementYPower = relativeYToPoint / (Math.abs(relativeXToPoint) + Math.abs(relativeYToPoint));
+
+        movementXPower *= movementSpeed;
+        movementYPower *= movementSpeed;
+
+
+        double relativeTurnAngle = relativeAngleToPoint - Math.toRadians(180) + preferredAngle;
+
+        relativeTurnAngle /= MathFunctions.Clip(relativeTurnAngle/Math.toRadians(30), -1, 1) * turnSpeed;
+
+        if(distanceToTarget < 10) { // If is is closer than 10 cm reduce the turning otherwise there is orbiting
+            relativeTurnAngle *= 0;
+        }
+         //todo: get the data for the movement from here
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Crawler/core/utils/MathFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Crawler/core/utils/MathFunctions.java
@@ -1,19 +1,88 @@
 package org.firstinspires.ftc.teamcode.Crawler.core.utils;
 
+import java.util.ArrayList;
+
 public class MathFunctions {
 
     /**
-     * Makes shore an angel is within the range -180 to 180 degrees
-     * @param angle
-     * @return
+     * Makes sure an angle is within the range -180 to 180 degrees
      */
-
     public static double AngleWrap(double angle) {
 
-        while(Math.toRadians(angle) < -Math.PI) angle += 2 * Math.PI;
-
-        while(Math.toRadians(angle) > Math.PI) angle -= 2 * Math.PI;
+        while (angle < -180) angle += 360;
+        while (angle > 180) angle -= 360;
 
         return angle;
+    }
+
+    public static double Clip(double value, double min, double max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static ArrayList<Point> lineCircleIntersection(
+            Point circleCenter,
+            double radius,
+            Point linePointA,
+            Point linePointB
+    ) {
+
+        if (Math.abs(linePointA.y - linePointB.y) < 0.003) {
+            linePointA.y = linePointB.y + 0.003;
+        }
+
+        if (Math.abs(linePointA.x - linePointB.x) < 0.003) {
+            linePointA.x = linePointB.x + 0.003;
+        }
+
+        double m1 = (linePointB.y - linePointA.y) / (linePointB.x - linePointA.x);
+
+        double quadraticA = 1.0 + Math.pow(m1, 2);
+
+        double x1 = linePointA.x - circleCenter.x;
+        double y1 = linePointA.y - circleCenter.y;
+
+        double quadraticB = (2.0 * m1 * y1) - (2.0 * Math.pow(m1, 2) * x1);
+
+        double quadraticC = (Math.pow(m1, 2) * Math.pow(x1, 2))
+                - (2.0 * y1 * m1 * x1)
+                - Math.pow(radius, 2);
+
+        ArrayList<Point> allPoints = new ArrayList<>();
+
+        try {
+            double discriminant = Math.pow(quadraticB, 2) - (4.0 * quadraticA * quadraticC);
+
+            if (discriminant < 0) return allPoints; // no intersection
+
+            double sqrt = Math.sqrt(discriminant);
+
+            double xRoot1 = (-quadraticB + sqrt) / (2.0 * quadraticA);
+            double yRoot1 = m1 * (xRoot1 - x1) + y1;
+
+            xRoot1 += circleCenter.x;
+            yRoot1 += circleCenter.y;
+
+            double minX = Math.min(linePointA.x, linePointB.x);
+            double maxX = Math.max(linePointA.x, linePointB.x);
+
+            if (xRoot1 > minX && xRoot1 < maxX) {
+                allPoints.add(new Point(xRoot1, yRoot1));
+            }
+
+            double xRoot2 = (-quadraticB - sqrt) / (2.0 * quadraticA);
+            double yRoot2 = m1 * (xRoot2 - x1) + y1;
+
+            xRoot2 += circleCenter.x;
+            yRoot2 += circleCenter.y;
+
+            if (xRoot2 > minX && xRoot2 < maxX) {
+                allPoints.add(new Point(xRoot2, yRoot2));
+            }
+
+        } catch (Exception e) {
+            // silently ignore (could log if needed)
+        }
+
+        return allPoints;
     }
 }
